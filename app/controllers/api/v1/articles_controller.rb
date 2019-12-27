517005c9aed600b81882b7a2b3c387b7a3f4a4a1
Pankaj <pankaj15297@gmail.com>
2019-12-27 15:29:49 +0530

    Added create and destroy methods

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,6 +10,25 @@ module Api
 				article = Article.find(params[:id])
 				render json: {status: 'SUCCESS', message: 'Loaded article', data: article}, status: :ok
 			end
+
+			def create
+				article = Article.new(article_params)
+
+				if article.save
+					render json: {status: 'SUCCESS', message: 'Saved article', data: article}, status: :ok
+				else
+					render json: {status: 'ERROR', message: 'Article not saved', data: article.errors}, status: :unprocessable_entity
+				end
+			end
+			def destroy
+				article = Article.find(params[:id])
+				article.destroy
+				render json: {status: 'SUCCESS', message: 'Deleted article', data: article}, status: :ok
+			end
+			private
+			def article_params
+				params.permit(:title, :body)
+			end
 		end
 	end
 end
