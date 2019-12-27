38a1f9c28540abc2bfeec719425c622ce1508cb3
Pankaj <pankaj15297@gmail.com>
2019-12-27 18:50:05 +0530

    message

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,7 +3,7 @@ module Api
 		class ArticlesController < ApplicationController
 			def index
 				articles = Article.order('created_at DESC')
-				render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
+				render json: {status: 'SUCCESS', message: 'Loaded articles', data:articles}, status: :ok
 			end
 
 			def show
